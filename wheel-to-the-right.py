4861cd1d0c9560188fbf3403d86950a09ea92737
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-03-17 17:33:14 +0900

    爆発を追加

--- wheel-to-the-right.py
+++ wheel-to-the-right.py
@@ -11,11 +11,14 @@ class Enemy:
     x: int
     y: int
     exists: bool
+    exploded_at: Optional[int]
+    max_explode_radius: float
 
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
         self.exists = True
+        self.exploded_at = None
 
     def update(self, app: "App"):
         if (
@@ -23,12 +26,31 @@ class Enemy:
             and abs(app.player_x - self.x) < 6
             and abs(app.bullet_y - self.y) < 6
         ):
-            self.exists = False
+            self.exploded_at = pyxel.frame_count
             app.bullet_y = None
+            app.combo += 1
+            app.score += app.combo
+            self.max_explode_radius = 5 + 7 / app.combo
+
+        if self.exploded_at is not None and pyxel.frame_count - self.exploded_at > 10:
+            self.exists = False
+            self.exploded_at = None
+
+    @property
+    def exploding(self) -> bool:
+        return self.exploded_at is not None
+
+    def exploding_radius(self) -> float:
+        if self.exploded_at is None:
+            return 0
+        return (pyxel.frame_count - self.exploded_at) / 10 * self.max_explode_radius
 
 
 class App:
     phase: "Phase"
+    score: int
+    combo: int
+    stage: int
     enemies: List["Enemy"]
     player_x: float
     player_vx: float
@@ -37,7 +59,9 @@ class App:
     def __init__(self):
         self.bullet_y = None
         self.enemies = [Enemy(64, 32), Enemy(32, 16), Enemy(96, 16)]
-
+        self.score = 0
+        self.stage = 1
+        self.combo = 0
         self.phase = Ready(self)
         pyxel.init(128, 128, title="Hello Pyxel")
 
@@ -51,14 +75,20 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
+        pyxel.text(2, 2, f"Score{self.score:5d}", 7)
+        if self.combo:
+            pyxel.text(128 - 4 * 9, 2, f"{self.combo:3d} Combo", 7)
         for e in self.enemies:
-            if e.exists:
+            if e.exploding:
+                pyxel.circ(e.x, e.y, e.exploding_radius(), 8)
+            elif e.exists:
                 pyxel.rect(e.x - 3, e.y - 3, 6, 6, 14)
         if isinstance(self.phase, Fire) and self.bullet_y is not None:
             pyxel.rect(self.player_x - 2, self.bullet_y - 3, 4, 6, 10)
         pyxel.rect(self.player_x - 4, 120, 8, 8, 6)
         if isinstance(self.phase, Ready):
-            pyxel.text(64 - (4 * 13 - 1) / 2, 64 - 5 / 2, "[F] to start!", 7)
+            pyxel.text(64 - 4 * 9 / 2, 80, f"Stage:{self.stage:3d}", 7)
+            pyxel.text(64 - 4 * 22 / 2, 80 + 10, "Hold [Space] to start!", 7)
 
 
 class Ready(Phase):
@@ -67,7 +97,7 @@ class Ready(Phase):
         app.player_vx = 0
 
     def update(self, app: "App") -> "Phase":
-        if pyxel.btnp(pyxel.KEY_F):
+        if pyxel.btnp(pyxel.KEY_SPACE):
             return Moving(app)
         return self
 
@@ -77,7 +107,7 @@ class Moving(Phase):
         pass
 
     def update(self, app: "App") -> "Phase":
-        if pyxel.btn(pyxel.KEY_F):
+        if pyxel.btn(pyxel.KEY_SPACE):
             app.player_vx = min(app.player_vx + 0.1, 1.5)
             app.player_x += app.player_vx
             return self
@@ -102,9 +132,11 @@ class Stop(Phase):
 
 
 class Fire(Phase):
+    stopped_frame: int
 
     def __init__(self, app: "App"):
         app.bullet_y = 128
+        app.combo = 0
 
     def update(self, app: "App") -> "Phase":
         if app.bullet_y is not None:
@@ -113,9 +145,12 @@ class Fire(Phase):
                 app.bullet_y = None
         for e in app.enemies:
             e.update(app)
-        if app.bullet_y is None:
-            return Ready(app)
-        return self
+        if app.bullet_y is not None or any(e.exploding for e in app.enemies):
+            self.stopped_frame = pyxel.frame_count
+            return self
+        if pyxel.frame_count - self.stopped_frame < 20:
+            return self
+        return Ready(app)
 
 
 if __name__ == "__main__":
